016a28495db616da6a3e154f6831d28cc5e849c8
smita <dsmita22@gmail.com>
2019-07-17 15:25:09 +1000

    complete fileupload

---
+++ tests/fileupload.spec.js
@@ -0,0 +1,27 @@
+const puppeteer = require( 'puppeteer' );
+const path = require('path');
+jest.setTimeout( 60000 );
+
+describe('Test', () => {
+    let filePath;
+    it('Test file upload', async () => {
+        // filePath = path.join(__dirname,'../Smita_Dutta_Resume.docx');
+        
+        const browser =await puppeteer.launch({
+            headless: true,
+            slowMo: 300
+          });
+          const page = await browser.newPage();
+          const url ='https://www.livecareer.com/resume-check';
+          await page.goto(url);
+        
+          await page.waitForSelector('input[id="RadUpload1file0"]');
+          await page.click('input[id="RadUpload1file0"]');
+          const input = await page.$("input[type=file]");
+          await input.uploadFile(process.cwd()+ "\\Smita_Dutta_Resume.docx");
+          
+          await page.screenshot({path:'upload.png'});
+
+    await browser.close();
+    });
+});
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,5 +1,7 @@
 const puppeteer = require( 'puppeteer' );
 
+
+
 jest.setTimeout( 60000 );
 
 /**describe( 'Test search engines', () => {
@@ -21,20 +23,36 @@ jest.setTimeout( 60000 );
         await browser.close();
     });
 }); */
+describe.skip('Test', () => {
+ 
+    it('', async () => {
+        const browser =await puppeteer.launch({
+            headless: false
 
-test ('', async () => {
-    const browser =await puppeteer.launch({
-        headless: false
     });
+  
     const page = await browser.newPage();
-    const url ='https://www.makemytrip.com/';
+    //await page.setViewport()
+    const url ='https://www.gumtree.com.au/t-login-form.html?pr=true';
     await page.goto(url);
-    await page.click('div[class="makeFlex column flexOne whiteText latoBold"]');
-    await page.click('a[class="latoBlack"]');
-    await page.type('input[id="emailOrPhone"]','test@gmail.com' );
-    await page.type('input[id="password"]','test@123' );
-    await page.click('button[class="capText font16"]');
+
+    await page.type('input[id="login-email"]','dsmita22@gmail.com');
+    await page.waitForSelector('input[id="login-password"]',{visible:true});
+    await page.type('input[id="login-password"]','Test@123456789');
+    await page.click('button[id="btn-submit-login"]');
+   // await page.waitForSelector('a[class="header__post-ad-button new-listing-button h-url"]',{visible:true});
+    await page.click('#header-new > div > div.header__top-layer > div.header__upper-deck-wrapper > div.header__desktop-post-ad-button.header__mobile-post-ad-button--cars-landing.mobile-menu-items-wrapper > div > a');
+    await page.type('input[id="title"]','dinning table');
+    await page.click('a[id="cat_18397"]');
+  //  await page.type('input[id="pstad-price"]','20');
+
+  
+
+    await page.click('button[id="btn-register"]');
 
     
     await browser.close();
+
+
+});
 });
